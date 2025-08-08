d3a4dec6fec347991d7c3521f3c0739294aa6818
BasilSabu <141615788+BasilSabu1@users.noreply.github.com>
2025-08-08 17:40:51 +0530

    components updated

---
+++ components/apiconfig/api_urls.js
@@ -0,0 +1,23 @@
+export const API_URLS = {
+  
+    PAYMENT: {
+      POST_PAYMENT: "api/create-payment/",
+      
+    },
+  
+    VERIFY_PAYMENT: {
+      POST_VERIFY_PAYMENT: "api/payment/verify/",
+      
+    },
+  
+    DEMO: {
+      POST_DEMO: "api/request-demo/",
+      
+    },
+  
+  
+ 
+  
+ 
+  };
+  
